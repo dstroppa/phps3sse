120b26afc083d8df1a2209a0550022e111fcbaec
Daniele Stroppa <stroppad@amazon.com>
2014-10-16 13:45:49 +0100

    Initial commit

--- recipes/appsetup.rb
+++ recipes/appsetup.rb
@@ -13,6 +13,15 @@ node[:deploy].each do |app_name, deploy|
     EOH
   end
 
+  s3 = AWS::S3.new(
+      :access_key_id => 'AKIAIQTW7PHF2SMDIOCA',
+      :secret_access_key => '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')
+  secret = s3.buckets['stroppad-chef-data-bags'].objects['encrypted_data_bag_secret'].read.strip
+
+  file "/etc/chef/encrypted_data_bag_secret" do
+    content secret
+  end
+
   template "#{deploy[:deploy_to]}/current/db-connect.php" do
     source "db-connect.php.erb"
     mode 0660
@@ -24,13 +33,7 @@ node[:deploy].each do |app_name, deploy|
       owner "apache"
     end
 
-    s3 = AWS::S3.new(
-      :access_key_id => 'AKIAIQTW7PHF2SMDIOCA',
-      :secret_access_key => '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')
-    secret = s3.buckets['stroppad-chef-data-bags'].objects['encrypted_data_bag_secret'].read.strip
-
-    rdspwd = Chef::EncryptedDataBagItem.load("rds_secrets", "rdspwd", secret)
-    Chef::Log.info("The decrypted object is '#{rdspwd}' ")
+    rdspwd = Chef::EncryptedDataBagItem.load("rds_secrets", "rdspwd")
     Chef::Log.info("The decrypted user is '#{rdspwd['user']}' ")
     Chef::Log.info("The decrypted password is '#{rdspwd['password']}' ")
 
